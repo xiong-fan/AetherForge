6720f3871aeefb4d885b75f554971178578fe935
tangao <tangao@tangaodeMacBook-Pro.local>
2026-04-29 20:57:18 +0800

    update next.config.mjs

--- web3-dapp/next.config.mjs
+++ web3-dapp/next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, {isServer}) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+
+    if(!isServer){
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        '@react-native-async-storage/async-storage': false,  // 或指向一个空实现
+      };
+    }
+
     return config
   },
 }
